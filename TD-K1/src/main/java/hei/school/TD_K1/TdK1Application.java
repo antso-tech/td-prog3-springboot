4eda946be9e5c92a8a7c2b99a560be8c1dece355
antso.tech <hei.antso.2007@gmail.com>
2026-03-19 08:30:59 +0300

    feat: ajout de java springboot

---
+++ TD-K1/src/main/java/hei/school/TD_K1/TdK1Application.java
@@ -0,0 +1,13 @@
+package hei.school.TD_K1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdK1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdK1Application.class, args);
+	}
+
+}
---
+++ TD-K1/src/test/java/hei/school/TD_K1/TdK1ApplicationTests.java
@@ -0,0 +1,13 @@
+package hei.school.TD_K1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdK1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
