a0d14c96ff986ee5897b8597b8da31e1654fc845
antso.tech <hei.antso.2007@gmail.com>
2026-03-19 09:12:47 +0300

    fix: ajout de la requete pour la création des étudiants

--- TD-K1/src/main/java/hei/school/TD_K1/Controller/StudentContoller.java
+++ TD-K1/src/main/java/hei/school/TD_K1/Controller/StudentContoller.java
@@ -1,16 +1,23 @@
 package hei.school.TD_K1.Controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import hei.school.TD_K1.Entity.StudentEntity;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
-@RequestMapping("/")
+@RequestMapping
 public class StudentContoller {
+    List<StudentEntity> studentsList = new ArrayList<>();
 
     @GetMapping("/welcome")
     public String getWelcomeMessage(@RequestParam String name) {
@@ -18,10 +25,9 @@ public class StudentContoller {
     }
 
     @PostMapping("/student")
-    public String postMethodName(@RequestBody String entity) {
-        //TODO: process POST request
-        
-        return entity;
+    public List<StudentEntity> postMethodName(@RequestBody List<StudentEntity> students) {
+        studentsList.addAll(students);
+        return studentsList;
     }
     
     
--- TD-K1/src/main/java/hei/school/TD_K1/Service/StudentService.java
+++ TD-K1/src/main/java/hei/school/TD_K1/Service/StudentService.java
@@ -1,5 +1,6 @@
 package hei.school.TD_K1.Service;
 
 public class StudentService {
+    public record getStudentService(String reference, String firstName, String lastName, int age){}
     
 }
