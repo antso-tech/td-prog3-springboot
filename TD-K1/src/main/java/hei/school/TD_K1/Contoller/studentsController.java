ec5bb4743082159cf4a0f8bb174d6876017b6cde
antso.tech <hei.antso.2007@gmail.com>
2026-03-20 16:02:33 +0300

    feat: ajout de getWelcome

--- TD-K1/src/main/java/hei/school/TD_K1/Contoller/studentsController.java
+++ TD-K1/src/main/java/hei/school/TD_K1/Contoller/studentsController.java
@@ -1,8 +1,26 @@
 package hei.school.TD_K1.Contoller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+
+@RequestMapping
+@RestController
 public class studentsController {
+    @GetMapping("/welcome")
+    public ResponseEntity<String> welcomeUser(@RequestParam String name) {
+        if(name != null){
+            return ResponseEntity.status(200).body("Welcome " + name);
+
+        }else{
+            return ResponseEntity.status(400).body("Vous devez fournir un nom");
+        }
+    }
     
+
 
     
 }
--- TD-K1/src/main/java/hei/school/TD_K1/Entity/StudentEntity.java
+++ TD-K1/src/main/java/hei/school/TD_K1/Entity/StudentEntity.java
@@ -1,12 +1,16 @@
 package hei.school.TD_K1.Entity;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @AllArgsConstructor
 @Getter
 @Setter
+@ToString
+@EqualsAndHashCode
 public class StudentEntity {
     private String references;
     private String firsName;
