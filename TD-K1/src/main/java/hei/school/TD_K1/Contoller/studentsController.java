ef9008dc1560933e803739dd030f8fecb6590942
antso.tech <hei.antso.2007@gmail.com>
2026-03-20 16:10:59 +0300

    feat: ajout de createAllUsers

--- TD-K1/src/main/java/hei/school/TD_K1/Contoller/studentsController.java
+++ TD-K1/src/main/java/hei/school/TD_K1/Contoller/studentsController.java
@@ -2,14 +2,24 @@ package hei.school.TD_K1.Contoller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import hei.school.TD_K1.Entity.StudentEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RequestMapping
 @RestController
 public class studentsController {
+
+    List<StudentEntity> studentsList = new ArrayList<>();
+
     @GetMapping("/welcome")
     public ResponseEntity<String> welcomeUser(@RequestParam String name) {
         if(name != null){
@@ -19,8 +29,14 @@ public class studentsController {
             return ResponseEntity.status(400).body("Vous devez fournir un nom");
         }
     }
-    
 
+     public ResponseEntity<List<StudentEntity>> getAllStudents(@RequestBody List<StudentEntity> studentEntity){
+        studentsList.addAll(studentEntity);
+        return ResponseEntity.status(201).body(studentsList);
 
+     }
+
+     
     
 }
+
