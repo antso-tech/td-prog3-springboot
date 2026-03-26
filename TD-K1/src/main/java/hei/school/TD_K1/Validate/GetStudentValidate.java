850c07d23cb9d98a6191dbc1a0c70162837496d6
antso.tech <hei.antso.2007@gmail.com>
2026-03-26 08:00:27 +0300

    amélioration du code getStudentValidate

--- TD-K1/src/main/java/hei/school/TD_K1/Validate/GetStudentValidate.java
+++ TD-K1/src/main/java/hei/school/TD_K1/Validate/GetStudentValidate.java
@@ -3,14 +3,13 @@ package hei.school.TD_K1.Validate;
 import org.springframework.stereotype.Component;
 
 import hei.school.TD_K1.Exception.StudentException;
-import hei.school.TD_K1.Exception.UnsupportedMediaTypeException;
 
 @Component
 public class GetStudentValidate {
     public void getStudentHeaderValidate(String acceptRequest){
    
        
-        if (acceptRequest == null || acceptRequest.trim().isEmpty() ) {
+        if (acceptRequest == null || acceptRequest.trim().isEmpty() || acceptRequest.contains("*/*")) {
             throw new StudentException("Le header Accept doit être requis");
             
         }else{
